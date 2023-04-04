f287a3756ddc2ae186f9d608228e1abba1271141
Sai gowtham <117100926+p-sai-gowtham@users.noreply.github.com>
2023-04-04 19:51:21 +0530

    Update app.js

--- random/app.js
+++ random/app.js
@@ -1,6 +1,17 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+
 const email = document.getElementById('email');
+
 const pass = document.getElementById('pass');
 
 const button = document.getElementById('button').addEventListener('click', () => {
-  
+    createUserWithEmailAndPassword(auth, email.value, pass.value);
+    .then((response) => { 
+        console.log(response);
+    })
+    .catch((error) => {
+        alert(error);
+    });
 });

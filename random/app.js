bb3d23a817e8d5e94afe410f4de7caf8a30d4ced
Sai gowtham <117100926+p-sai-gowtham@users.noreply.github.com>
2023-04-04 18:39:21 +0530

    Add files via upload

---
+++ random/app.js
@@ -0,0 +1,6 @@
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+
+const button = document.getElementById('button').addEventListener('click', () => {
+  
+});
